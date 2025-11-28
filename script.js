624c1c0410f53dc72e70f0ba59486cbd1d6112b5
omarfaruk8617 <omarfaruk8617@gmail.com>
2025-11-28 16:55:21 +0100

    init

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function sayHello() {
   alert("hello world 🎉");
+function loadQuote() {
+  fetch('https://dummyjson.com/quotes/random')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('quote').innerText = data.quote;
+    })
+    .catch(error => {
+      document.getElementById('quote').innerText = "Could not load quote 😢";
+      console.error("API error:", error);
+    });
 }
+
+loadQuote();
